c63a32f8e91e85090e0fa8efe9081224909a0e80
gracenak <gracenak@gmail.com>
2020-10-13 15:06:45 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,44 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        if logged_in?
+            @tweets = Tweet.all
+            @user = current_user
+            erb :"tweets/index"
+        else
+            redirect "/login" 
+        end
+    end
+
+    get '/tweets/new' do
+        erb :new
+    end
+
+    get '/tweets/:id' do
+        if logged_in?
+            @tweet = Tweet.find(params[:user_id])
+            erb :"users/show"
+        else
+            redirect "/login"    
+            
+        end
+    end
+
+    get '/tweets/new' do
+        if logged_in?
+        erb :"tweets/new"
+        end
+    end
+
+    post '/tweets/new' do
+      
+
+    end
+
+
+    
+
+
+
 
 end
---
+++ db/migrate/20201011054236_create_tweets_table.rb
@@ -0,0 +1,7 @@
+class CreateTweetsTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.string :content
+    end
+  end
+end
