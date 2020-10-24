4a2043a0590a698e9a6774f1e01b0a2175164b53
github username <github email address>
2020-10-23 22:13:45 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,32 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :sessions_secret, "secret"
   end
 
-end
+  get "/" do
+    erb :home
+  end 
+
+
+
+  helpers do 
+
+    def logged_in?
+      !!current_user
+    end 
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end 
+
+    def authentication_required
+      if !logged_in?
+        flash[:message] = "You must be logged in."
+        redirect '/'
+      end
+    end
+  end
+  
+end 
