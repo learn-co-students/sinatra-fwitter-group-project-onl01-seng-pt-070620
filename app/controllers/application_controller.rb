0af92e91326cb250a13625a40e19104fd148980c
Keesha Hargrove <kharg14@gmail.com>
2020-10-26 13:26:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,28 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "top_secret"
   end
+      
+    get '/' do
+         erb :home
+   end 
+
+
+    
+  helpers do
+    
+    def current_user
+      User.find(session[:user_id])
+    end 
+    
+    def is_logged_in?
+        !!session[:user_id]
+    end
+  end 
+
+
 
 end
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,54 @@
 class UsersController < ApplicationController
 
+  get '/' do  
+      redirect '/tweets' if is_logged_in?
+      erb :index
+  end 
+
+  get '/signup' do
+    if is_logged_in?
+      redirect '/tweets'
+    else 
+      erb :'users/signup'
+    end 
+  end 
+  
+  post '/signup' do
+    user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+    if user.save && user.username != "" && user.email != "" && user.password != ""
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect to '/signup'
+    end
+  end
+  
+  get '/login' do 
+    if is_logged_in?
+        redirect to '/tweets'
+    else 
+      erb :'users/login' 
+    end
+  end 
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+     # binding.pry
+      redirect to "/tweets"
+    end 
+  end 
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to '/login'
+  end
+
 
 end
