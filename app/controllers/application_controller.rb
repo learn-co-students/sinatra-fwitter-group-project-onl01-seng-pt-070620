280405f0562745ac63056e0729675a08a644e96c
eulis01 <eulis01@gmail.com>
2020-11-19 01:33:46 -0500

    edit app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,24 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    enable :sessions
-    set :session_secret, "fwitter_secret"
+    
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
   end
 
   get '/' do
     erb :index
   end
+  helpers do
+    
+    def logged_in?
+      !!current_user
+    end
 
-  # Helper Methods
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
-
-  def logged_in?
-    !!current_user
+    def current_user
+      @current_user ||=  User.find_by(id: session[:user_id]) if session[:user_id]
+    end
   end
 end
